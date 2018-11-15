d4e78ef38402dbb31d8ffbfd0c47568a0e52ff9a
Tyler Chargin <chargint.sp3@sslmda.net>
2018-11-15 12:24:28 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 1337;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function (req,res) {res.status(200).send('Test'); });
+
+app.listen(port, function() {
+	console.log('listening on port ' + port);
+});
+
+app.post('/hello', function (req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text : 'Hello ' + userName + ', welcome to Mystic Slack channel! I\'ll be your guide.'
+  };
+  // Loop otherwise..
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+});
